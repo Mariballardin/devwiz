9f443d7f295afd4af3c5fae9fa35cd5015683550
Mariana Ballardin <mariana.ballardin@hotmail.com>
2023-06-05 15:45:25 +0200

    route code_reviews, has one attached file, add button get started

--- app/models/code_review.rb
+++ app/models/code_review.rb
@@ -1,3 +1,4 @@
 class CodeReview < ApplicationRecord
   belongs_to :prompt
+  has_one_attached :file
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :prompts, only: [:index, :new, :create, :show]
+  resources :prompts, only: [:index, :new, :create, :show] do
+    resources :code_reviews, only: [:index, :new, :create]
+  end
+  resources :code_reviews, only: [:show]
 end
