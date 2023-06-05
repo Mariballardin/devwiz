3297d7e6ed679feec6a863b6ecd6bd94cfd092b9
Mariana Ballardin <mariana.ballardin@hotmail.com>
2023-06-05 16:20:14 +0200

    created index page, defined the method new and render the form

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -1,13 +1,26 @@
 class PromptsController < ApplicationController
+  before_action :set_prompt, only: [:show, :create]
+
   def index
   end
 
   def new
+    @prompt = Prompt.new
   end
 
   def show
   end
 
   def create
+  end
+
+  private
+
+  def set_prompt
+    @prompt = Prompt.find(params[:id])
+  end
+
+  def prompt_params
+    params.require(:prompt).permit(:programming_language, :job_title, :job_field, :key_values)
   end
 end
--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -1,5 +1,5 @@
 class Prompt < ApplicationRecord
-  LANGUAGE = %w(C C++ C# Go Java JavaScript Kotlin MATLAB PHP Python R Ruby Rust Swift TypeScript)
+  LANGUAGES = %w(C C++ C# Go Java JavaScript Kotlin MATLAB PHP Python R Ruby Rust Swift TypeScript)
   belongs_to :user
   has_many :code_reviews
 end
