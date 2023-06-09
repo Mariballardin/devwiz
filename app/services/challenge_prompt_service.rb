2c3bba2da01a74dfd5aac81853b21c4b7789d88b
Mariana Ballardin <mariana.ballardin@hotmail.com>
2023-06-09 13:13:52 +0200

    my challenges page is done

--- app/services/challenge_prompt_service.rb
+++ app/services/challenge_prompt_service.rb
@@ -15,7 +15,7 @@ class ChallengePromptService
     ranging from 1 (beginner) to 5 (advanced). The challenge should require only one file to be coded.
     The company operates in the #{@challenge_prompt.job_field} field.
     Please, divide the challenge in bullets separated by 4 dashes and topics:
-    First the challenge with 2 bullets points: one with the challenge itself 
+    First the challenge with 2 bullets points: one with the challenge itself
     and another one saying the difficulty level.
     After, Requirements, with 6 topics numered and each topic with the respectives bullet points.
     Lastly, write a 'Bonus features'
@@ -30,7 +30,7 @@ class ChallengePromptService
           messages: [{ role: "user", content: build_challenge_prompt }], # Required.
           temperature: 0.7,
           stream: false,
-					max_tokens: 250 # might want to check this
+					max_tokens: 550 # might want to check this
       })
     # you might want to inspect the response and see what the api is giving you
     return response["choices"][0]["message"]["content"]
