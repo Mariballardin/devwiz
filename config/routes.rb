a83999fe97105d654245c97c95be377ac0eb8c03
Mariana Ballardin <mariana.ballardin@hotmail.com>
2023-06-13 16:15:51 +0200

    destroy method

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :prompts, only: [:index, :new, :create, :show] do
+  resources :prompts, only: [:index, :new, :create, :show, :destroy] do
     resources :code_reviews, only: [:index, :new, :create]
   end
   resources :code_reviews, only: [:show]
